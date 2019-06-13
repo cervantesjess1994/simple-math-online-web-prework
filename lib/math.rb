930387402ee11cb4284a13f393a562e9a4fe773e
Jessica Cervantes-Torres <cervantesjess1994@gmail.com>
2019-06-13 11:04:36 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,27 +1,27 @@
-def addition
-  puts 5+4
+def addition(num1, num2)
+  puts "num1+num2"
 end
 
 def subtraction(num1, num2)
-  puts num1-num2
+  puts "num1-num2"
 end
 
 def division(num1, num2)
-  puts num1/num2
+  puts "num1/num2"
 end
 
 def multiplication(num1, num2)
-  puts num1*num2
+  puts "num1*num2)"
 end
 
 def modulo(num1, num2)
-  puts Math
+  puts "Math"
 end
 
 def square_root(num)
-  puts Math.sqrt(num)
+  puts "Math.sqrt(num)""
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  puts num1+((num2*num3)/num4)
+  puts "num1+((num2*num3)/num4)""
 end
